5f70e101fc8b4530d3d1cdf3ab32df818fba4867
Jonathan-RX <jonathan.ravix@gmail.com>
2021-07-14 09:53:37 +0200

    Feature/Invoice: Configuration of InvoiceType

--- src/Form/InvoiceType.php
+++ src/Form/InvoiceType.php
@@ -2,8 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Customer;
 use App\Entity\Invoice;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +15,19 @@ class InvoiceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('Customer', EntityType::class,[
+                'class' => Customer::class,
+                'choice_label' => 'name',
+            ])
             ->add('adress')
-            ->add('content')
-            ->add('sold')
-            ->add('printed')
-            ->add('send_by_mail')
-            ->add('created_at')
             ->add('email')
-            ->add('Customer')
-            ->add('created_by')
+            ->add('content', CollectionType::class,[
+                'entry_type' => ContentType::class,
+                'entry_options' => ['label' => false],
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false
+            ])
         ;
     }
 
